39bf8889b9380658dab4b7d904acf9cc6f2d7cc4
Felipe Morais <moraisfelipe97@gmail.com>
2019-10-13 11:43:43 -0300

    Initial version

---
+++ Shape Grammar/Assets/ResetScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ResetScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+}
---
+++ Shape Grammar/Assets/Shape Grammar/Rule.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rule : MonoBehaviour
+{
+
+    public GameObject rule;
+
+    void Start()
+    {
+        //Debug.Log(name);
+
+        if (rule != null)
+        {
+            GameObject obj = nextRule(rule);
+            if (obj != null)
+            {
+                GameObject next = Instantiate(obj) as GameObject;
+                next.transform.parent = transform;
+                resetTransform(next);
+            }
+            
+        }
+        
+        
+
+
+    }
+
+
+    public GameObject nextRule(GameObject rule)
+    {
+        
+        List<GameObject> rules = new List<GameObject>();
+        foreach (Transform t in rule.transform)
+        {
+            rules.Add(t.gameObject);
+        }
+        if (rules.Count > 0)
+            return rules[Random.Range(0, rules.Count)];
+        return null;
+
+    }
+
+    public void resetTransform(GameObject obj)
+    {
+        obj.transform.localPosition = Vector3.zero;
+        obj.transform.localRotation = Quaternion.identity;
+        obj.transform.localScale = Vector3.one;
+    }
+
+
+}
